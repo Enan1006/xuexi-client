7286e3e9287ae6bf9e9433a1604cc154a8f2ed90
Imdadul Haque Enan <ihenan1006@gmail.com>
2022-08-20 00:55:54 +0600

    services component added

--- src/Components/Service/Service.js
+++ src/Components/Service/Service.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
-const Service = () => {
+const Service = (props) => {
+    const { _id, img, service, description } = props.service;
     return (
         <div>
+            <figure><img className='mx-auto' src={img} alt={service} /></figure>
+            <div class="card-body">
+                <h2 class="card-title">{service}</h2>
+                <p>{description}</p>
+                <div class="card-actions justify-end">
+                    <button class="btn btn-primary">Buy Now</button>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Services/Services.js
+++ src/Components/Services/Services.js
@@ -2,9 +2,24 @@ import React, { useEffect, useState } from 'react';
 import Service from '../Service/Service';
 
 const Services = () => {
-
+    const [services, setServices] = useState([]);
+    useEffect(() => {
+        fetch('http://localhost:5000/services')
+            .then(Response => Response.json())
+            .then(data => setServices(data))
+    }, [])
     return (
-        <div>
+        <div className='mx-20 my-20'>
+            <h1 className='text-4xl text-blue-600 font-semibold text-center mb-10'>Services</h1>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+                {
+                    services.map(service => <Service
+                        service={service}
+                        key={service._id}
+                    >
+                    </Service>)
+                }
+            </div>
         </div>
     );
 };
